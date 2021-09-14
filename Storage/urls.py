35298ee47d19a84c39f58c8cd03c20dc44bc336e
Cranchian <pramod.ijjarothu@bennett.edu.in>
2021-09-14 16:47:48 +0530

    changed single product url base from /<slug> to /book/<slug>

--- Storage/urls.py
+++ Storage/urls.py
@@ -6,6 +6,6 @@ app_name = 'Storage'
 
 urlpatterns = [
     path('', views.product_all, name='product_all'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('book/<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
